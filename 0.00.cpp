cd071a055cf6ffed4ec5785721b84c8ebc3617fa
AdelNejati <152187850+AdelNejati@users.noreply.github.com>
2024-01-10 20:54:29 +0330

    first table

--- 0.00.cpp
+++ 0.00.cpp
@@ -1,9 +1,42 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main(){
-    int j;
-    cin>>j;
-    cout<<j;
+    int x , y;
+    cout << "Enter number of lines : ";
+    cin >> x;
+    cout << "Enter number of columns : ";
+    cin >> y;
+    int table[x][y];
+    for(int i = 1 ; i <= x*2  + 1; i++){
+        cout << endl;   
 
-    return 0;
+        if(i%2 != 1){
+
+        
+     
+        for(int j = 1 ; j <= y*2 + 1 ; j++){
+            if(j%2 == 1){
+                if(j==1){
+                cout << "|  ";
+                }
+                else{
+                cout << "  |  ";
+                }
+            }
+            else{
+                cout << 0;
+            }
+        }
+        // cout<<"\n";
+        }
+        else{
+            for(int j = 1 ; j <= y*6 + 1 ; j++){
+                cout << "-";
+
+            } 
+            // cout<<"\n";
+        }
+
+    }
+
 }
